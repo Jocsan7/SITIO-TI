1b1ec8ec01bbc2c4d27246402d2d41b42a8e4d7c
A.Jocsan <240486@utxicotepec.edu.mx>
2026-01-08 14:29:13 -0600

    Crear funciones js y estilo de encabezado

--- JS/funciones.js
+++ JS/funciones.js
@@ -1,3 +1,27 @@
 function saludar(){
     alert("Bienvenido a la Academia de T.I");
 }
+function suma(){
+    var valor1 = 24;
+    var valor2 = 13;
+    var resultado = valor1 + valor2;
+    alert("Resultado de la suma de " +valor1+ " y " +valor2+ " es: " +resultado)
+}
+function resta(){
+    var valor1 = 24;
+    var valor2 = 13;
+    var resultado = valor1 - valor2;
+    alert("Resultado de la resta de " +valor1+ " y " +valor2+ " es: " +resultado)
+}
+function mul(){
+    var valor1 = 5;
+    var valor2 = 10;
+    var resultado = valor1 * valor2;
+    alert("Resultado de la multiplicación de " +valor1+ " y " +valor2+ " es: " +resultado)
+}
+function div(){
+    var valor1 = 24;
+    var valor2 = 12;
+    var resultado = valor1 / valor2;
+    alert("Resultado de la división de " +valor1+ " y " +valor2+ " es: " +resultado)
+}
